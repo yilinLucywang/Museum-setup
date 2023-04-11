6883e430dde51c258e80de389c76587a4f57bb68
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-04-11 18:35:00 -0400

    add rotation feature

---
+++ Assets/scenes/scripts/PressController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine.EventSystems;
+using System; 
+
+public class PressController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
