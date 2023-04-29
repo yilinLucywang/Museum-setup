5380e3b124cc055ff2cf316c6de51b0a5f7884f7
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-04-29 16:43:05 -0400

    change

--- Assets/controlScripts/WrongCnt.cs
+++ Assets/controlScripts/WrongCnt.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Runtime.CompilerServices;
 public static class WrongCnt
 {
     public static int wrongCnt = 0; 
