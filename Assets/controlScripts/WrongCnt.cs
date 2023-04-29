1fe861570defb029f0942934ef99fe045ea7d1a4
yilinLucywang <yilinwa2@andrew.cmu.edu>
2023-04-29 17:22:47 -0400

    change

--- Assets/controlScripts/WrongCnt.cs
+++ Assets/controlScripts/WrongCnt.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.CompilerServices;
+
 public static class WrongCnt
 {
-    public static int wrongCnt = 0; 
-
+    //public static WrongCnt WrongCnt = null;              
+    
+    public static int wrongCnt = 0;
     public static void addCnt(){
-        wrongCnt += 1;
-    }
+       wrongCnt += 1;
+   }
+   //Awake is always called before any Start functions
+   // void Awake()
+   // {
+   //     // //Check if instance already exists
+   //     // if (WrongCnt == null)
+           
+   //     //     //if not, set instance to this
+   //     //     WrongCnt = this;
+       
+   //     // //If instance already exists and it's not this:
+   //     // else if (WrongCnt != this)
+           
+   //     //     //Then destroy this. This enforces our singleton pattern, 
+   //     //     // meaning there can only ever be one instance of a GameManager.
+   //     //     Destroy(gameObject);    
+       
+   //     // //Sets this to not be destroyed when reloading scene / Switching scenes
+   //     // DontDestroyOnLoad(gameObject); // VERY IMPORTANT
+       
+   // }
 }
